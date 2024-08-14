f14680f675536a4358392975f45365890e384504
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-15 01:45:44 +0300

    Android.mk: Protect with TARGET_USES_AUDIOREACH flag
    It so happened that we have a new Audioreach based HAL and legacy
    one (pre SM8450 SoC)  at the same time. They use the same module
    names, so we must protect them to compile the required one and
    prevent conflicts.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_USES_AUDIOREACH), true)
 ifneq ($(AUDIO_USE_STUB_HAL), true)
 ifeq ($(TARGET_USES_QCOM_MM_AUDIO), true)
 
@@ -7,5 +8,6 @@ include $(MY_LOCAL_PATH)/hal/Android.mk
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
 include $(MY_LOCAL_PATH)/audio-effects/Android.mk
 
+endif
 endif
 endif
