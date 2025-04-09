0668e10bd3482e98f7bf2b90a401307f4f0e2b5d
Karthik D K <quic_kdk@quicinc.com>
2025-04-09 17:06:49 +0530

    hal: Fix compilation errors on Nord W
    - reorder initialization as per mem variable declared
    - remove by reference and make copy of object to
      fix dangling reference
    
    Change-Id: I8ec5051b8490e7bcbcb08c1a6727e69d9f85c63d
    Signed-off-by: Karthik D K <quic_kdk@quicinc.com>

--- hal/core/platform/AudioUsecase.cpp
+++ hal/core/platform/AudioUsecase.cpp
@@ -766,7 +766,7 @@ int64_t CompressPlayback::getPositionInFrames(pal_stream_handle_t* palHandle) {
     uint64_t sessionTimeUs =
             ((static_cast<decltype(sessionTimeUs)>(tstamp.session_time.value_msw)) << 32 |
              tstamp.session_time.value_lsw);
-    const auto& sampleRate = getSampleRate(mMixPortConfig).value();
+    const auto sampleRate = getSampleRate(mMixPortConfig).value();
     // sessionTimeUs to frames
     // try to convert the session to frames without loss of precision.
     mPrevFrames = static_cast<int64_t>((sessionTimeUs / 1000) * sampleRate / 1000);
@@ -832,7 +832,7 @@ int64_t PcmOffloadPlayback::getPositionInFrames(pal_stream_handle_t* palHandle) 
     uint64_t sessionTimeUs =
             ((static_cast<decltype(sessionTimeUs)>(tstamp.session_time.value_msw)) << 32 |
              tstamp.session_time.value_lsw);
-    const auto& sampleRate = getSampleRate(mMixPortConfig).value();
+    const auto sampleRate = getSampleRate(mMixPortConfig).value();
     // sessionTimeUs to frames
     // try to convert the session to frames without loss of precision.
     mPrevFrames = static_cast<int64_t>((sessionTimeUs / 1000) * sampleRate / 1000);
