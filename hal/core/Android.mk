de13e591641292904ba96153e597577a32aec218
Pavel Dubrova <pashadubrova@gmail.com>
2026-03-26 11:56:34 +0200

    hal: Use VINTF fragment from sun config
    The fragment contains AIDL interfaces that are fully
    suitable for us.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- hal/core/Android.mk
+++ hal/core/Android.mk
@@ -18,7 +18,7 @@ LOCAL_CFLAGS := \
     -Wthread-safety
 
 LOCAL_VINTF_FRAGMENTS := \
-    ../../configs/$(TARGET_BOARD_PLATFORM)/manifest_audio_qti_services.xml
+    ../../configs/sun/manifest_audio_qti_services.xml
 
 LOCAL_SRC_FILES := \
     CoreService.cpp \
